5b50717b94f32850ef1993d03d74eea23ff9ebcf
jr051323 <jeffrey.rajkumar@cerner.com>
2020-04-11 22:48:09 +0530

    Progressbar and Test modules added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quiz';
+  public Reg: number = 0;
+  public Reg_Percentage: number = 0;
+  public Game: number = 0;
+  public Game_Percentage: number = 0;
+  public Result: number = 0;
+  public Result_Percentage: number = 0;
+  public n: number = 1;
+
+  public timeout() {
+    setTimeout(() => {
+      if (this.Reg < 100){
+        this.Reg = this.Reg + 1;
+        this.Reg_Percentage= this.Reg/3
+      } else if(this.Game < 100){
+        this.Game = this.Game + 1;
+        this.Game_Percentage= this.Game/3
+      } else if(this.Result < 100){
+        this.Result = this.Result + 1;
+        this.Result_Percentage= this.Result/3
+      }
+      
+      if (this.Reg < 100 || this.Game < 100 || this.Result < 100){
+        this.timeout();
+      }
+    }, 100);
+  }
+
+  constructor() {
+    console.log(`Fun1 Jef`);
+    let decimal: number=1000;
+
+    this.timeout();
+
+    console.log(`Fun1 Jef2`);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
